b40b0a54859acbd99f3758418282e022600a2407
Julio N <juhunuque@gmail.com>
2016-09-30 14:37:15 -0600

    Commit

--- public/js/app.js
+++ public/js/app.js
@@ -11,6 +11,10 @@ app.config(['$routeProvider',function($routeProvider){
     templateUrl: 'js/html/statusES.view.html',
     controller: 'StatusESCtrl'
 })
+.when('/statusAdmin',{
+    templateUrl: 'js/html/statusAdmin.view.html',
+    controller: 'StatusAdminCtrl'
+})
 .otherwise({redirectTo: 'home'})
 }
 ]);
--- public/js/controllers/statusAdmin.controller.js
+++ public/js/controllers/statusAdmin.controller.js
@@ -1,10 +1,11 @@
 angular.module("dgApp")
 
-.controller('StatusESCtrl',['$scope','$http',function($scope, $http){
-    console.log('StatusESCtrl Init...');
+.controller('StatusAdminCtrl',['$scope','$http',function($scope, $http){
+    console.log('StatusAdminCtrl Init...');
 
     function refresh(){
       $scope.title = '';
+      $scope.body = '';
       $scope.isStatusFormActive = false;
       console.log('Refresh');
     };
@@ -17,12 +18,21 @@ angular.module("dgApp")
       switch (opt) {
           case 0:
               $scope.title = 'Root endpoint result';
+              if(!$scope.isStatusFormActive){
+                makeRequest('');
+              }
               break;
           case 1:
               $scope.title = 'Status endpoint result';
+              if(!$scope.isStatusFormActive){
+                makeRequest('status');
+              }
               break;
           case 2:
               $scope.title = 'Healthcheck endpoint result';
+              if(!$scope.isStatusFormActive){
+                makeRequest('healthcheck');
+              }
               break;
           default:
               console.log('Error executing endpoint');
