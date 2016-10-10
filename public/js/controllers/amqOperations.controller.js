31400e5075a7f8b8cfbe00b51209bd1054a34f91
Julio N <juhunuque@gmail.com>
2016-10-10 11:24:29 -0600

    Commit

--- public/js/controllers/amqOperations.controller.js
+++ public/js/controllers/amqOperations.controller.js
@@ -1,5 +1,33 @@
 angular.module("dgApp")
 
-.controller('AmqOperationsCtrl',['$scope','$http', function($scope, $http){
+.controller('AmqOperationsCtrl',['$scope','$http', 'DTOptionsBuilder', 'Notification', '$dataDg', function($scope, $http, DTOptionsBuilder, Notification, $dataDg){
+  var configs = {};
+  $scope.tableMsg = "Loading queues";
+
+  $scope.dtOptions = DTOptionsBuilder.newOptions()
+      .withDisplayLength(10)
+      .withOption('bLengthChange', false)
+      .withOption('scrollY', "500px")
+      .withOption('scrollCollapse', true)
+      .withOption('oLanguage', {"sEmptyTable": $scope.tableMsg })
+      .withOption('autoWidth', true);
+
+  $scope.refresh = function(){
+    configs = $dataDg.getConfig();
+    $scope.objects = {};
+    $scope.tableMsg = "Loading subscriptions";
+
+    $http.get('/queuelist').then(function(response){
+            $scope.objects = response.data.queues;
+            Notification.success({title:'Success', message:'Queues loaded.'});
+            $scope.tableMsg = "No data available";
+          }, function(error){
+            Notification.error({title:'Error getting queues', message:'Check the console and try again.'});
+            $scope.tableMsg = "Error getting queues";
+            console.error('ERROR => ' + JSON.stringify(error.data));
+          });
+    };
+
+    $scope.refresh();
 
 }]);
